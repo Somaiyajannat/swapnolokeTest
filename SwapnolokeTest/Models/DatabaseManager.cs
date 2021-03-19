924719fcd1d767834597acf053833d4460542f1b
Somaiyajannat <somaiyajannat044@gmail.com>
2021-03-19 15:30:34 +0600

    adding connection string to DatabaseManager

--- SwapnolokeTest/Models/DatabaseManager.cs
+++ SwapnolokeTest/Models/DatabaseManager.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +9,14 @@ namespace SwapnolokeTest.Models
 {
     public class DatabaseManager
     {
+        private String connectionString = ConfigurationManager.ConnectionStrings["swapnolokeDB"].ConnectionString;
+        public SqlConnection connection = new SqlConnection();
+        public SqlCommand command = new SqlCommand();
 
+        DatabaseManager()
+        {
+            connection.ConnectionString = connectionString;
+            command.Connection = connection;
+        }
     }
 }
